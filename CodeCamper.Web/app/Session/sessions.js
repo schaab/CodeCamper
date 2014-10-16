47af68317a96125b37459364504f7846c2beb68a
Jared Schaab <jared.schaab@gmail.com>
2014-10-16 15:17:40 -0700

    Adding initial code for sessions

---
+++ CodeCamper.Web/app/Session/sessions.js
@@ -0,0 +1,28 @@
+﻿(function () {
+    'use strict';
+    var controllerId = 'sessions';
+    angular.module('app').controller(controllerId, ['common', sessions]);
+
+    function dashboard(common, datacontext) {
+        var getLogFn = common.logger.getLogFn;
+        var log = getLogFn(controllerId);
+
+        var vm = this;
+        
+        vm.activate = activate;
+        vm.sessions = [];
+        vm.title = 'Sessions';
+        
+
+        activate();
+        function activate() {
+            
+            common.activateController([getSessions], controllerId)
+                .then(function () { log('Activated Sessions View'); });
+        }
+
+        function getSessions() {
+           
+        }
+    }
+})();
